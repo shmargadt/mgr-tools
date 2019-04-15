087bf2cfd43da3855ae1357c049faef74d35d566
Tomer Shmargad <shmargadt@gmail.com>
2019-04-15 08:40:00 +0300

    Update setup.py

--- trello/setup.py
+++ trello/setup.py
@@ -33,7 +33,6 @@ def is_good_response(resp):
 def log_error(e):
     """
     It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
+    This function just prints them.
     """
     print(e)
