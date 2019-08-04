5ded2f90c79f5d9cb2efd69703d61ca45ff42130
Tomer Shmargad <tomer_shmargad@Tomers-MacBook-Pro.local>
2019-08-04 14:34:56 +0300

    github tasks

--- github/show_my_pr_status.py
+++ github/show_my_pr_status.py
@@ -6,10 +6,8 @@ import pprint
 pp = pprint.PrettyPrinter(depth=6)
 
 GITHUB_V3_URL = 'https://api.github.com'
-CLIENT_ID = sys.argv[1] 
-CLIENT_SECRET = sys.argv[2] 
-
-def get_my_repos(personal_key, personal_token):
+ACCESS_TOKEN = sys.argv[1]
+def get_my_repos(personal_token):
     """Get all repos of user
 
     Parameters:
@@ -21,13 +19,44 @@ def get_my_repos(personal_key, personal_token):
         Or raising an error if http is failing
 
     """
-    github_url = GITHUB_V3_URL + '/user/repos?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    print(github_url)
+    github_url = GITHUB_V3_URL + '/user/repos?access_token=' + personal_token
     response = simple_get(github_url)
-    print(response)
-    return response
+    github_repos_as_json = json.loads(response)
+    return github_repos_as_json
 
     # Raise an exception if we failed to get any data from the url
-    raise Exception('Error retrieving contents at {}'.format(trello_url))
+    raise Exception('Error retrieving contents at {}'.format(github_url))
 
-get_my_repos(CLIENT_ID, CLIENT_SECRET)
+
+def get_pulls_of_a_repo(repo_name, personal_token):
+    """Get all repos of user
+
+    Parameters:
+        client_id (string): 
+        client_secret (string): 
+
+    Returns:
+        repos (dict): 
+        Or raising an error if http is failing
+
+    """
+    github_url = GITHUB_V3_URL + '/repos/' + repo_name + '/pulls?access_token=' + personal_token
+    response = simple_get(github_url)
+    github_pulls_as_json = json.loads(response)
+    return github_pulls_as_json
+
+    # Raise an exception if we failed to get any data from the url
+    raise Exception('Error retrieving contents at {}'.format(github_url))
+
+repos = get_my_repos(ACCESS_TOKEN)
+
+pulls_in_repo = get_pulls_of_a_repo(repos[6]["full_name"], ACCESS_TOKEN)
+for pull_request in pulls_in_repo: 
+    creator = pull_request["user"]["login"]
+    if creator in ['ronysh']:
+        pp.pprint(pull_request['title'] + ", state: " + pull_request['state'])
+
+
+pulls_in_repo = get_pulls_of_a_repo(repos[3]["full_name"], ACCESS_TOKEN)
+for pull_request in pulls_in_repo: 
+    pp.pprint(pull_request['title'] + ", state: " + pull_request['state'])
